d92d7113a88b1e986c5af3be01fac8be70f14303
Bilel-707 <Bilel.bouneb90@gmail.com>
2023-02-01 00:23:00 +0100

    header done

--- client/src/PortfolioContent/Home/Home.js
+++ client/src/PortfolioContent/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./Header/Header/Header";
 import Profile from "./Profile/Profile";
 import Footer from "./Footer/Footer";
 import "./Home.css";
@@ -6,6 +7,7 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="home-content">
+      <Header />
       <Profile />
       <Footer />
     </div>
